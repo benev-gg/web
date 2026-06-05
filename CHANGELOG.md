566632262010ea6883e9baadc5f4d00c6de0b045
Chase Moskal <chasemoskal@gmail.com>
2026-06-04 21:37:10 -0700

    initial: commit

---
+++ CHANGELOG.md
@@ -0,0 +1,15 @@
+
+# changelog
+- 🟥 breaking change
+- 🔶 deprecation or possible breaking change
+- 🍏 harmless addition, fix, or enhancement
+
+
+
+<br/><br/>
+
+## v0.1
+
+### v0.1.0
+- 🍏 initial release
+
---
+++ s/index.html.ts
@@ -0,0 +1,34 @@
+
+import {template, html, socialCard, dataSvgEmoji} from "@e280/scute"
+
+const favicon = "/assets/favicon.png"
+
+export default template(import.meta.url, async orb => html`
+	<!doctype html>
+	<html>
+		<head>
+			<meta charset="utf-8"/>
+			<meta name="viewport" content="width=device-width,initial-scale=1"/>
+			<meta name="darkreader-lock"/>
+			<style>@layer base{html{background:#000;color:#aaa;}}</style>
+
+			<title>@benev/web</title>
+			<link rel="icon" href="${favicon}"/>
+			<link rel="icon" href="${dataSvgEmoji("😇")}"/>
+			<script type="module" src="${orb.hashurl("main.bundle.min.js")}"></script>
+
+			<style data-theme>${orb.inject("css/vars.css")}</style>
+
+			${socialCard({
+				themeColor: "#ffdd22",
+				siteName: "@benev/web",
+				title: "@benev/web",
+				description: "benev web ui library",
+			})}
+		</head>
+		<body>
+			<h1>benev</h1>
+		</body>
+	</html>
+`)
+
