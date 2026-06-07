a41ca2dd0e63b442bc77c2a68ba87ca52e08201e
Chase Moskal <chasemoskal@gmail.com>
2026-06-07 15:10:45 -0700

    fix: exports

--- s/index.ts
+++ s/index.ts
@@ -1,12 +1,13 @@
 
+export * from "./brains/loader.js"
 export * from "./brains/shell.js"
 
 export * from "./css/basis.css-text.js"
 export * from "./css/benev-core.css-text.js"
 export * from "./css/theme.css.js"
 
-export * from "./brains/loader.js"
+export * from "./dom/header/component.js"
+export * from "./dom/header/view.js"
 export * from "./dom/loader/component.js"
-
-export * from "./dom/shell/component.js"
+export * from "./dom/menu/component.js"
 
